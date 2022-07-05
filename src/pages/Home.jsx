ab6ea007c7695b8223b3b1904b715906035996af
Michael Sell <michael.sell@usa.net>
2022-07-05 16:52:03 +0200

    Fetch users from Github - initial version in component plus Spinner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import UserResults from '../components/users/UserResults';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <UserResults />
+    </>
+  );
 }
 
 export default Home;
