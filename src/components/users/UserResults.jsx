4210fdb1926b6074c03cba6a367a5a117657b450
Michael Sell <michael.sell@usa.net>
2022-07-07 17:14:57 +0200

    UserResults and userItems with Fetch API

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 function UserResults() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+  const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
-    });
+    const response = await fetch(`${GITHUB_URL}/users`);
+    console.log(`${GITHUB_TOKEN}`);
 
     const data = await response.json();
     setUsers(data);
@@ -25,7 +28,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
